c7100fe957527877a20ff8922573cb948fdfbff7
Corentin <corentin.huibant@laposte.net>
2017-01-31 08:53:43 +0100

    le quatrième exercice : chaine de chiffres où Fizz remplace les multiples de trois, Buzz les multiples de cinq et Fizzbuzz, les multiples de trois ET de cinq.

--- exercice4.php
+++ exercice4.php
@@ -2,14 +2,15 @@
   for($i = 0; $i < 301; $i++) {
     if($i % 3 == 0) {
       echo "Fizz";
-      echo "Fizzbuzz";
     }
     elseif($i % 5 == 0) {
       echo "Buzz";
+      }
+    elseif($i % 5 == 0 AND $i % 3 == 0) {
       echo "Fizzbuzz";
     }
     else {
       echo $i;
     }
-    }
-  ?>
+  }
+?>
